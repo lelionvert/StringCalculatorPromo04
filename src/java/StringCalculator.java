f5a9549c5860db9b0639da399a4b504fe47d576f
alexandre pouget <pouget.al@gmail.com>
2018-01-25 17:18:13 +0100

    refacto : clean code of add method in string calculator

--- src/java/StringCalculator.java
+++ src/java/StringCalculator.java
@@ -1,11 +1,13 @@
 public class StringCalculator {
     public int add(String input) {
-        if (input.length() <= 0)
+        if(input.length()<1){
             return 0;
+        }
         String[] split = input.split(",");
         int result = 0;
-        for (int i = 0; i < split.length; ++i)
-            result += Integer.parseInt(split[i]);
+        for (String s: split) {
+            result += Integer.parseInt(s);
+        }
         return result;
     }
 }
